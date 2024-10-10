bb4ce57b5802c3873b6fa1d615e08bf7a3d9ddeb
Salif youin <sigomapdevelopper@gmail.com>
2024-10-10 12:36:31 +0000

    Configuration pour ignorer les fichier

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  // apiUrl : 'http://localhost:5000/',
-     apiUrl : 'https://groupensia.synchronre.com:5000/',
- // apiUrl : 'http://groupensia-dev.synchronre.com:5002/',
-  // apiUrl : 'https://groupensia-test.synchronre.com:5001/'
-};
