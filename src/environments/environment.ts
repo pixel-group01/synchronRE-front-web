34e8b7903c8bad3340df32e9171052bc44ff2165
Salif youin <sigomapdevelopper@gmail.com>
2025-01-13 20:20:24 +0000

    Ajout des fichier env a la dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-    apiUrl : 'http://localhost:5000/',
-    //  apiUrl : 'http://137.74.199.79:5000/',
+   // apiUrl : 'http://localhost:5000/',
+  // apiUrl : 'http://137.74.199.79:5000/',//prod non seccurise
+  apiUrl : 'http://137.74.199.79:5002/',//dev non seccurise
    //  apiUrl : 'https://groupensia.synchronre.com:5000/',
   // apiUrl : 'http://groupensia-dev.synchronre.com:5002/',
   // apiUrl : 'https://groupensia-test.synchronre.com:5001/'
