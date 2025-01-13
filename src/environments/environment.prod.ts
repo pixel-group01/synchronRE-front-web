4ac1a5956156c90f1c063749cc606d4829976804
Salif youin <sigomapdevelopper@gmail.com>
2025-01-13 18:41:24 +0000

    Ajout des fichier env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  // apiUrl : 'http://localhost:5000/',
+  apiUrl : 'http://137.74.199.79:5000/',
+   //  apiUrl : 'https://groupensia.synchronre.com:5000/',
+ // apiUrl : 'http://groupensia-dev.synchronre.com:5002/',
+  // apiUrl : 'https://groupensia-test.synchronre.com:5001/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+ //  apiUrl : 'http://localhost:5000/',
+    apiUrl : 'http://137.74.199.79:5000/',
+    //  apiUrl : 'https://groupensia.synchronre.com:5000/',
+  // apiUrl : 'http://groupensia-dev.synchronre.com:5002/',
+  // apiUrl : 'https://groupensia-test.synchronre.com:5001/'
+};
