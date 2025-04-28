1fd3c3ccab88a81955cbc95ec9efdccecdfa5488
Salif youin <sigomapdevelopper@gmail.com>
2025-04-28 15:11:05 +0000

    config env test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,7 @@
 export const environment = {
   production: false,
  //  apiUrl : 'http://localhost:5000/',
-  //apiUrl : 'http://137.74.199.79:5000/',//prod non seccurise
-  apiUrl : 'http://137.74.199.79:5002/',//dev non seccurise
     //  apiUrl : 'https://groupensia.synchronre.com:5000/',
-  // apiUrl : 'http://groupensia-dev.synchronre.com:5002/',
-  // apiUrl : 'https://groupensia-test.synchronre.com:5001/'
+ // apiUrl : 'http://groupensia-dev.synchronre.com:5002/',
+   apiUrl : 'https://groupensia-test.synchronre.com:5001/'
 };
