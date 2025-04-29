52a58a36c81a2fec2a5005fc526a82b69deda3cd
PIXEL-GROUP <129598955+pixel-group01@users.noreply.github.com>
2025-04-29 13:38:05 +0000

    Update environment.ts
    Corection du fichier env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,6 @@ export const environment = {
  //  apiUrl : 'http://localhost:5000/',
     //  apiUrl : 'https://groupensia.synchronre.com:5000/',
  // apiUrl : 'http://groupensia-dev.synchronre.com:5002/',
-   apiUrl : 'https://groupensia-test.synchronre.com:5001/'
+  apiUrl : 'http://137.74.199.79:5001/',//test non seccurise
+  // apiUrl : 'https://groupensia-test.synchronre.com:5001/'
 };
