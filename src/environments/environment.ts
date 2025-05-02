73b441fc406126bbc4724ff71f2e5e60517963ee
Salif youin <sigomapdevelopper@gmail.com>
2025-05-02 11:36:15 +0000

    config env JL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
- //  apiUrl : 'http://localhost:5000/',
-  //apiUrl : 'http://137.74.199.79:5000/',//prod non seccurise
-  apiUrl : 'http://137.74.199.79:5002/',//dev non seccurise
-    //  apiUrl : 'https://groupensia.synchronre.com:5000/',
+  // apiUrl : 'http://localhost:5000/',
+      apiUrl : 'https://groupensia.synchronre.com:5000/',
   // apiUrl : 'http://groupensia-dev.synchronre.com:5002/',
   // apiUrl : 'https://groupensia-test.synchronre.com:5001/'
 };
