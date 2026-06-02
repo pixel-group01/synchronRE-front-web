69a8f2d77df4736a55710e73d1b69de225b04a34
salifyouin <39273318+salifyouin@users.noreply.github.com>
2026-06-02 15:36:46 +0000

    Update environment.ts
    https vers http

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   // apiUrl : 'http://localhost:5000/',
     //apiUrl : 'https://groupensia.synchronre.com:5000/',
-  apiUrl : 'http://groupensia.synchronre.com:5000/',
+  apiUrl : 'http://137.74.199.79:5000/',
   // apiUrl : 'http://groupensia-dev.synchronre.com:5002/',
   // apiUrl : 'https://groupensia-test.synchronre.com:5001/'
 };
